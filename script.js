533ca73bb3d17cf624919c0df749c0d13c23f584
MiniNetflix <openaibusiness22@gmail.com>
2025-07-08 15:32:34 +0200

    Update script.js

--- script.js
+++ script.js
@@ -27,7 +27,7 @@ const seriesData = {
 const moviesData = {
   "Il Padrino": {
     img: "https://via.placeholder.com/150x220?text=Il+Padrino",
-    src: "https://www.youtube.com/embed/sY1S34973zA"
+    src: "https://supervideo.cc/oz82xaij80cd"
   },
   "Inception": {
     img: "https://via.placeholder.com/150x220?text=Inception",
